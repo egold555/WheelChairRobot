2e6adc851ec9c07cef8282da918ffc9a4de65b3a
egold555 <eric@golde.org>
2018-06-03 15:34:41 -0700

    Added ai chatbot thingy

---
+++ ChatBotConsoleTest/ChatBotConsoleTest/Program.cs
@@ -0,0 +1,62 @@
+﻿using AIMLbot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatBotConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new ChatBot().run();
+        }
+    }
+
+    class ChatBot
+    {
+        const string UserId = "user";
+        private Bot AimlBot;
+        private User myUser;
+
+        public void run()
+        {
+            Console.WriteLine("Program > Loading bot...");
+            AimlBot = new Bot();
+            myUser = new User(UserId, AimlBot);
+
+            AimlBot.loadSettings();
+            AimlBot.isAcceptingUserInput = false;
+            AimlBot.loadAIMLFromFiles();
+            AimlBot.isAcceptingUserInput = true;
+            Console.WriteLine("Bot > Hi");
+            while (true)
+            {
+                var input = Console.ReadLine();
+               
+                if (input != null && input.Length != 0)
+                {
+                    if(input == "exit")
+                    {
+                        
+                        break;
+                    }
+                    Console.WriteLine("User > " + input);
+                    String output = getOutput(input);
+                    Console.WriteLine("Bot > " + output);
+                    Console.WriteLine(" ");
+                }
+
+            }
+        }
+
+        private String getOutput(String input)
+        {
+            Request r = new Request(input, myUser, AimlBot);
+            Result res = AimlBot.Chat(r);
+            return (res.Output);
+        }
+    }
+}
